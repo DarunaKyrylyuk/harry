508cb7550d288ec1ffa2a43ac9a035d653ca3179
DarunaKyrylyuk <kyrylyuk.d2303@lntu.edu.ua>
2025-05-19 14:12:50 +0300

    Delete Search.js

--- Search.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("searchBtn").addEventListener("click", function () {
-  let query = document.getElementById("searchInput").value.toLowerCase();
-  alert("Пошук за запитом: " + query);
-});
